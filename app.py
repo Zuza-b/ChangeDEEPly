8f0c082212f3c9d11c4ff5ab9259002e5fbfdd26
shilpa reddy <shilpa.madhurig@gmail.com>
2022-03-11 11:03:34 +0100

    final changes

--- app.py
+++ app.py
@@ -18,13 +18,15 @@ from sklearn.ensemble import RandomForestClassifier
 import joblib
 
 
+from streamlit_option_menu import option_menu
+
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 #st.set_page_config(layout="wide")
 
+
 class App():
     # This code is different for each deployed app.
-    #CURRENT_THEME = "blue"
-    #IS_DARK_THEME = True
+
     #EXPANDER_TEXT = """
     #This is a custom theme. You can enable it by copying the following code
     #to `.streamlit/config.toml`:
@@ -37,6 +39,15 @@ class App():
     #font = "sans-serif"
     #```
     #"""
+    st.set_page_config(
+                page_title='Streamlit Demonstration App',
+                # set Streamlit logotype as the favicon
+                #page_icon="Streamlit_Logo_1.jpg", # None, ":memo:...
+                #layout='wide', # centered, wide
+                #initial_sidebar_state='expanded' # auto, expanded, collapsed
+            )
+
+
     def __init__(self):
         #self.Age = Age
         #self.Education = Education
@@ -44,8 +55,11 @@ class App():
         #self.Gender = Gender
         pass
 
+
     def set_page(self):
         # Set border of current theme to red, otherwise black or white
+        #CURRENT_THEME = "blue"
+        #IS_DARK_THEME = True
         #border_color = "red"
         #border_color = "lightgrey" if IS_DARK_THEME else "black"
         '''
@@ -54,15 +68,15 @@ class App():
         #st.title('MOOC Dropout Prediction')
         MOOC_img = 'MOOC.jpg'
         #st.image(MOOC_img, width=350)
-
-        col1, col2 = st.columns([0.7,0.3])
-        with col2:
-            st.image(MOOC_img, width=350)
-        with col1:
-            st.title('MOOC Dropout Prediction')
+        with st.container():
+            col1, col2 = st.columns([0.7,0.3])
+            with col2:
+                st.image(MOOC_img, width=350)
+            with col1:
+                st.title('MOOC Dropout Prediction')
 
         content = 'Project to predict on dropout for students enrolled on MOOC(Massive Open Online Courses)'
-        st.markdown(f'<p style="color:#99ff88;font-size:35px;border-radius:2%;">{content}</p>', unsafe_allow_html=True)
+        st.markdown(f'<p style="color:#000080;font-size:35px;border-radius:5%;">{content}</p>', unsafe_allow_html=True)
 
         #st.header('Project to predict on dropout for students enrolled on MOOC(Massive Open Online Courses)')
         #f'<p style="background-color:#0066cc;color:#77ff33;font-size:40px;border-radius:2%;">{content}</p>'
@@ -103,23 +117,22 @@ class App():
 
 
     def behavour_predict(self):
-        #model2 = joblib.load('KNN_shilpa_model_0.joblib')
-        #X = dict(
-
-        #birth=50,
-        #category='economics',
-        #gender='male',
-        #education='High'
-        #)
-        #output2 = model2.predict(X)
-
-        #if output2 == 1:
-        #    out2 = 'dropout of'
-        #else:
-        #    out2 = 'complete'
+        output2 = [1,0,1,0]
+        list(output2)
+        user_col = ['user_1','user_2','user_3', 'user_4']
+        list1 = []
+        for i in output2:
+            if i == int(1):
+                list1.append('dropout')
+            else:
+                list1.append('complete')
+        data_tuples = list(zip(user_col,list1))
+        o = pd.DataFrame(data_tuples, columns=['Username','Most Probable to'])
+        st.write(o)
 
 
-        st.subheader(f'This student model has to be loaded ')
+
+        #st.subheader(f'This student model has to be loaded ')
 
     def read_csv(self):
 
@@ -143,14 +156,14 @@ class App():
         st.button('click here to predict',on_click = self.behavour_predict)
         #st.image('/home/shilpa/code/Zuza-b/ChangeDEEPly/action_counts_img.png')
 
-    def sidebar(self):
+    def student_info(self):
 
-        #side bar
-        with st.sidebar.form(key='my_form'):
+
+        with st.form(key='my_form'):
             st.subheader('Student Personal Info')
 
-            Age  = st.slider('How old are you?', 0, 130, 23)
-
+            #Age  = st.slider('How old are you?', 0, 130, 23)
+            Age = st.number_input("birth year")
             Gender = st.selectbox('🧑 pick your gender',('male', 'female'))
 
 
@@ -171,15 +184,6 @@ class App():
             submit_button = st.form_submit_button(label='Calculate!',
             on_click=self.form_callback, args=(Age, Gender, Education, category ))
 
-            file  = st.file_uploader("Upload file", type=['csv'])
-            if file is not None:
-            #st.write('hi')
-                file_details = {"FileName":file.name,"FileType":file.type,"FileSize":file.size}
-                st.write(file_details)
-                extension = file.name.split('.')[1]
-                if extension.upper() == 'CSV':
-                    df = pd.read_csv(file)
-                    st.write(df.head())
             #return Age, Gender, Education, category
 
     def form_callback(self, Age, Gender, Education, category):
@@ -211,9 +215,8 @@ class App():
         else:
             out = 'complete'
 
-
-        st.subheader(f'This student is most probable to {out} the course ')
-
+        st.subheader(f'This student is most probable to {out} the course')
+        #st.write('here is your output')
 
         #st.button('upload file?')
 
@@ -222,8 +225,25 @@ class App():
 if __name__ == '__main__':
 
     app = App()
-    app.set_page()
+    #app.set_page()
     #Age, Gender, Education, category = app.sidebar()
-    app.sidebar()
+    #app.student_info()
     #app.form_callback(Age, Gender, Education, category)
-    app.read_csv()
+    #app.read_csv()
+
+    with st.sidebar:
+        selected = option_menu("MOOC Menu", ["Models", 'Student_PI', 'Instructor'])
+        st.sidebar.title("About")
+        st.sidebar.write(
+        """
+        This is an app to predict if a student is a dropout or not from an online training.
+        Created with passion for learning. Created for you to succeed, to learn and grow.
+        Created by a team of dedicated and skilful data scientists.
+        """
+    )
+    if selected == "Models":
+        app.set_page()
+    if selected == 'Student_PI':
+        app.student_info()
+    if selected == 'Instructor':
+        app.read_csv()
